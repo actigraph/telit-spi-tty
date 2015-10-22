6770cd6d1ced5d2ec4dd2d57ba0aac729f5acd70
Judge Maygarden <jmaygarden@gmail.com>
2015-10-22 11:05:39 -0500

    Found ifx6x60 driver to use as a baseline. Saving work before switching to it.

--- telit-spi-tty.c
+++ telit-spi-tty.c
@@ -27,50 +27,80 @@ MODULE_AUTHOR("Judge Maygarden");
 MODULE_DESCRIPTION("TTY emulation over SPI with Telit xE910 cellular modems.");
 MODULE_LICENSE("GPL");
 
-#define TELIT_SPI_TTY_MINORS 1
-static struct tty_driver *telit_spi_tty_driver;
+#define DRVNAME     "telit_spi"
+#define TTYNAME     "ttyTELIT"
 
-/*********** BEGIN tty_operations ***********/
+struct tty_driver *telit_tty_drv;
+
+#define TELIT_SPI_TTY_MINORS 1
+static struct telit_spi_tty_driver telit_spi;
+
+/*********** begin tty operations ***********/
 
 static int
-telit_spi_tty_open(struct tty_struct * tty, struct file * filp)
+telit_spi_open(struct tty_struct * tty, struct file * filp)
 {
+    printk(KERN_INFO "%s(0x%p, 0x%p)\n", __func__, tty, filp);
+
     return 0;
 }
 
 static void
-telit_spi_tty_close(struct tty_struct * tty, struct file * filp)
+telit_spi_close(struct tty_struct * tty, struct file * filp)
 {
+    printk(KERN_INFO "%s(0x%p, 0x%p)\n", __func__, tty, filp);
 }
 
 static int
-telit_spi_tty_write_room(struct tty_struct *tty)
+telit_spi_write_room(struct tty_struct *tty)
 {
+    printk(KERN_INFO "%s(0x%p)\n", __func__, tty);
+
     return 0;
 }
 
 static int
-telit_spi_tty_write(struct tty_struct * tty,
+telit_spi_write(struct tty_struct * tty,
                     const unsigned char *buf,
                     int count)
 {
+    printk(KERN_INFO "%s(0x%p, 0x%p, %d)\n", __func__, tty, buf, count);
+
     return 0;
 }
 
 static void
-telit_spi_tty_wait_until_sent(struct tty_struct *tty, int timeout)
+telit_spi_wait_until_sent(struct tty_struct *tty, int timeout)
 {
+    printk(KERN_INFO "%s(0x%p, %d)\n", __func__, tty, timeout);
 }
 
 static struct tty_operations telit_spi_ops = {
-    .open            = telit_spi_tty_open,
-    .close           = telit_spi_tty_close,
-    .write           = telit_spi_tty_write,
-    .write_room      = telit_spi_tty_write_room,
-    .wait_until_sent = telit_spi_tty_wait_until_sent
+    .open            = telit_spi_open,
+    .close           = telit_spi_close,
+    .write           = telit_spi_write,
+    .hangup          = telit_spi_hangup,
+    .write_room      = telit_spi_write_room,
+    .wait_until_sent = telit_spi_wait_until_sent
 };
 
-/*********** END   tty_operations ***********/
+/*********** end tty operations ***********/
+
+/*********** being spi operations ***********/
+
+struct spi_driver telit_spi_drv = {
+    .driver = {
+        .name = DRVNAME,
+        .pm = &telit_spi_pm,
+        .owner = THIS_MODULE
+    },
+    .probe = telit_spi_probe,
+    .shutdown = telit_spi_shutdown,
+    .remove = telit_spi_remove,
+    .id_table = telit_spi_id_table
+};
+
+/*********** end spi operations ***********/
 
 static int __init
 telit_spi_init(void)
@@ -79,42 +109,56 @@ telit_spi_init(void)
 
     printk(KERN_INFO "Loading telit_spi_tty...\n");
 
-    telit_spi_tty_driver = tty_alloc_driver(TELIT_SPI_TTY_MINORS, 0);
-    if (!telit_spi_tty_driver) {
+    telit_tty_driver = tty_alloc_driver(TELIT_SPI_TTY_MINORS, 0);
+    if (!telit_tty_driver) {
         return -ENOMEM;
     }
 
-    telit_spi_tty_driver->owner = THIS_MODULE;
-    telit_spi_tty_driver->driver_name = "telit_spi_tty";
-    telit_spi_tty_driver->name = "ttySPI";
-    telit_spi_tty_driver->major = 0;
-    telit_spi_tty_driver->type = TTY_DRIVER_TYPE_SERIAL;
-    telit_spi_tty_driver->subtype = SERIAL_TYPE_NORMAL;
-    telit_spi_tty_driver->flags = TTY_DRIVER_REAL_RAW | TTY_DRIVER_DYNAMIC_DEV;
-    tty_set_operations(telit_spi_tty_driver, &telit_spi_ops);
+    telit_tty_driver->owner = THIS_MODULE;
+    telit_tty_driver->driver_name = DRVNAME;
+    telit_tty_driver->name = TTYNAME;
+    telit_tty_driver->major = 0;
+    telit_tty_driver->minor_start = 0;
+    telit_tty_driver->type = TTY_DRIVER_TYPE_SERIAL;
+    telit_tty_driver->subtype = SERIAL_TYPE_NORMAL;
+    telit_tty_driver->flags = TTY_DRIVER_REAL_RAW | TTY_DRIVER_DYNAMIC_DEV;
+    telit_tty_driver->init_termios = tty_std_termios;
 
-    rv = tty_register_driver(telit_spi_tty_driver);
+    tty_set_operations(telit_tty_driver, &telit_spi_ops);
+
+    rv = tty_register_driver(telit_tty_driver);
     if (rv) {
-        printk(KERN_ERR "%s - tty_register_driver failed.\n", __func__);
-        put_tty_driver(telit_spi_tty_driver);
-        return rv;
+        printk(KERN_ERR "%s: tty_register_driver failed(%d)\n",
+                DRVNAME, rv);
+        goto err_free_tty;
     }
 
-    tty_register_device(telit_spi_tty_driver, 0, NULL);
+    rv = spi_register_driver(&telit_spi_driver);
+    if (rv) {
+        printk(KERN_ERR "%s: spi_register_driver failed(%d)\n",
+                DRVNAME, rv);
+        goto err_unreg_spi;
+    }
 
     return 0;
+
+err_unreg_spi:
+    spi_unregister_driver(&telit_spi_driver);
+err_free_tty:
+    put_tty_driver(telit_tty_driver);
+
+    return rv;
 }
+module_init(telit_spi_init);
 
 static void __exit
 telit_spi_exit(void)
 {
     printk(KERN_INFO "Unloading telit_spi_tty...\n");
 
-    tty_unregister_device(telit_spi_tty_driver, 0);
-    tty_unregister_driver(telit_spi_tty_driver);
-    put_tty_driver(telit_spi_tty_driver);
+    tty_unregister_driver(telit_tty_driver);
+    put_tty_driver(telit_tty_driver);
+    spi_unregister_driver(&telit_spi_driver);
 }
-
-module_init(telit_spi_init);
 module_exit(telit_spi_exit);
 
